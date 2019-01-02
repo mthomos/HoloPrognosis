c2f956be8e667757fe7e550cda11f912b3f66594
Marios Thomos <marios.thomos@embiots.gr>
2019-01-02 04:35:19 +0200

    FileManager

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    public FileManager fileManager;
+    private List<int> settings;
+    private bool trigerred1, trigerred2;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+        string settingsPath = Path.Combine(Application.persistentDataPath, "settings.txt");
+        if (File.Exists(settingsPath)) //First run setting.txt doesn't exist
+        {
+            File.Delete(settingsPath);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timer > 2 && !trigerred1)
+        {
+            fileManager.addString("settings.txt", "1 \n0 \n0 \n1 ");
+            trigerred1 = true;
+        }
+
+        if (Time.time - timer > 5 && !trigerred2)
+        {
+            string text = fileManager.readFile("settings.txt");
+            Debug.Log("TEXT------------" + text);
+            trigerred2 = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+#if UNITY_WSA && !UNITY_EDITOR
+using Windows.Storage;
+using System.Threading.Tasks;
+#endif
+
+public struct FileStruct
+{
+    public string fileName;
+    public string content;
+
+    public FileStruct (string fileName, string content)
+    {
+        this.fileName = fileName;
+        this.content = content;
+    }
+}
+
+public class FileManager : MonoBehaviour
+{
+    private static string settingsName = "settings.txt";
+
+    private static string appPath;
+    private static string output;
+    private static Queue<FileStruct> writeBuffer = new Queue<FileStruct>();
+
+    public static FileManager Instance { get; private set; }
+
+    private void Start()
+    {
+        if(appPath == null)
+            appPath = Application.persistentDataPath;
+
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        ProcessQueue();
+    }
+
+    private void ProcessQueue()
+    {
+        if (writeBuffer.Count > 0)
+        {
+            Debug.Log("Try to write");
+            FileStruct current = writeBuffer.Dequeue();
+            Task task = WriteFile(current);
+            task.Wait();
+        }
+    }
+
+    private static async Task WriteFile(FileStruct current)
+    {
+#if UNITY_WSA && !UNITY_EDITOR
+        StorageFolder folder = ApplicationData.Current.LocalFolder;
+        StorageFile file;
+        if (File.Exists(Path.Combine(appPath, current.fileName)))
+        {
+            file = await folder.GetFileAsync(current.fileName);
+            Debug.Log("File exist");
+        }
+        else
+        {
+            file = await folder.CreateFileAsync(current.fileName);
+            Debug.Log("File doesnt exist");
+        }
+        try
+        {
+            Debug.Log("Trying to write");
+            await FileIO.WriteTextAsync(file, current.content);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error writing in file->" + current.fileName + "_________" + e);
+            //Insert to buffer again
+            writeBuffer.Enqueue(current);
+        }
+#endif
+    }
+
+    public void addString(string file, string content)
+    {
+        writeBuffer.Enqueue(new FileStruct(file, content));
+    }
+
+    public string readFile(string fileName)
+    {
+        output=null;
+
+        if (!File.Exists(Path.Combine(appPath, fileName)))
+            return "Not_found";
+
+        Task task = ReadFile(fileName);
+        task.Wait();
+        return output;
+    }
+
+    private static async Task ReadFile(string fileName)
+    {
+#if UNITY_WSA && !UNITY_EDITOR
+        StorageFolder folder = ApplicationData.Current.LocalFolder;
+        StorageFile file = await folder.GetFileAsync(fileName);
+        try
+        {
+            output = await FileIO.ReadTextAsync(file);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error reading  file->" + fileName + "___" + e);
+        }
+#endif
+    }
+}
